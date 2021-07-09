23dd3260a815bf95576f88f017449f8d16be0681
anokhramesh <67797819+anokhramesh@users.noreply.github.com>
2021-07-09 22:22:30 +0400

    Add files via upload

---
+++ conversionC_F.py
@@ -0,0 +1,5 @@
+# This is a simple Program to Convert User input Centigrade to Fahrenheit.
+print("**************************************")
+c = float(input("Enter the Celsius value\n"))
+f = float(c * 9/5) + 32 
+print(c," \u00B0Celsius =",f,"\u00B0Fahrenheit")
